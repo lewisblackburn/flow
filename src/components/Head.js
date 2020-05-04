666c780ced0015a6ab70a28dc11761665eabdc09
zxffo <lewisblackburn10@gmail.com>
2020-05-04 21:30:30 +0100

    first commit

--- src/components/Head.js
+++ src/components/Head.js
@@ -4,7 +4,11 @@ export default function Head(props) {
   return (
     <div className="Navigation">
       <a href="https://www.themoviedb.org" target="_blank">
-        <img alt="" src="logo.svg" style={{ width: '80px', padding: '30px' }} />
+        <img
+          alt=""
+          src="./logo.svg"
+          style={{ width: '80px', padding: '30px' }}
+        />
       </a>
       <input
         onChange={props.searchChangeHandler}
