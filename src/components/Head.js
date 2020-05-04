5a21146b9ccdf870e6e16f4967b760a6f256e174
zxffo <lewisblackburn10@gmail.com>
2020-05-04 21:39:07 +0100

    new logo

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,14 +1,11 @@
 import React from 'react'
+import logo from '../images/logo.svg'
 
 export default function Head(props) {
   return (
     <div className="Navigation">
       <a href="https://www.themoviedb.org" target="_blank">
-        <img
-          alt=""
-          src="./logo.svg"
-          style={{ width: '80px', padding: '30px' }}
-        />
+        <img alt="" src={logo} style={{ width: '80px', padding: '30px' }} />
       </a>
       <input
         onChange={props.searchChangeHandler}
